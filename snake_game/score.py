99bde116d9a2da6ff92359d0dd207f61d083f47a
shaun zheng <shaun.123xyz@gmail.com>
2022-01-25 01:17:05 +1100

    snake game is working

---
+++ snake_game/score.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+
+
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=("Courier", 24, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER! Your final score:{self.score}", align=ALIGNMENT, font=("Courier", 16, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
