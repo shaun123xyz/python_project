4bd3ebd84b52a5cd93d296c78e1a6678057df090
shaun zheng <shaun.123xyz@gmail.com>
2022-01-26 00:51:50 +1100

    added highest score

--- snake_game/score.py
+++ snake_game/score.py
@@ -8,6 +8,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("highest_score.txt") as data:
+            self.highest_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -18,6 +20,10 @@ class Score(Turtle):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=("Courier", 24, "normal"))
 
     def game_over(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.goto(0, 0)
         self.write(f"GAME OVER! Your final score:{self.score}", align=ALIGNMENT, font=("Courier", 16, "normal"))
 
