5c66beb568127d38b526fb057a9c7e7c62810592
shaun zheng <shaun.123xyz@gmail.com>
2022-02-10 01:04:13 +1100

    added create password function

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,7 +1,20 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import string
 
 
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def create_password():
+    password_entry.delete(0, END)
+    password = [random.choice(string.ascii_letters).lower() for i in range(3)]
+    password += [random.randint(0,9) for i in range(4)]
+    password += [random.choice(string.ascii_letters).upper() for i in range(3)]
+    password += [random.choice(symbols) for i in range(4)]
+    random.shuffle(password)
+    password_entry.insert(0, password)
+    
+    
 window = Tk()
 window.title("Password Manager")
 window.config(padx=80, pady=80)
@@ -23,14 +36,19 @@ website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
-email_entry.insert(0, "youremail@gmail.com")
 password_entry = Entry(width=26)
 password_entry.grid(row=3, column=1, sticky="EW")
 
+
+
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=create_password)
 generate_password_button.grid(row=3, column=2, sticky="EW")
 add_button = Button(text="Add", width=36)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
+
+
+
+
