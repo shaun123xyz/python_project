6c620f6ef891d8e705ebe1d45afaff857b17deee
shaun zheng <shaun.123xyz@gmail.com>
2022-02-10 01:23:39 +1100

    able to save password

--- password_manager/main.py
+++ password_manager/main.py
@@ -8,13 +8,38 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 def create_password():
     password_entry.delete(0, END)
     password = [random.choice(string.ascii_letters).lower() for i in range(3)]
-    password += [random.randint(0,9) for i in range(4)]
+    password += [str(random.randint(0,9)) for i in range(4)]
     password += [random.choice(string.ascii_letters).upper() for i in range(3)]
     password += [random.choice(symbols) for i in range(4)]
     random.shuffle(password)
+    password = "".join(password)
     password_entry.insert(0, password)
     
-    
+
+
+def add_password():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you have entered the website")
+    elif len(email) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you have entered your email")
+    elif len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you have entered your password or generate a password")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nWebsite: {website}\nEmail: {email} "
+                                                      f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
+  
 window = Tk()
 window.title("Password Manager")
 window.config(padx=80, pady=80)
@@ -40,11 +65,10 @@ password_entry = Entry(width=26)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 
-
 # Buttons
 generate_password_button = Button(text="Generate Password", command=create_password)
 generate_password_button.grid(row=3, column=2, sticky="EW")
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=add_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
